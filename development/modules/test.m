4e30ee80ff1492b4057705861bc5c5d8864478ce
nickhpt <nickhpt@users.noreply.github.com>
2022-11-11 18:02:31 +0100

    Hello world

---
+++ development/modules/test.m
@@ -0,0 +1,15 @@
+close all; clear all; clc
+
+personsDynamicChange = struct('Nick',[],'Marcus',[],'Kasper',[],'Kevin',[]);
+
+%Add balance
+Nick = 0;
+Kevin = 0;
+Marcus = 0;
+Kasper = 0;
+
+Nick = 10;
+
+
+
+
---
+++ development/solution/RMV_SIXTY_EIGHT/RMV_SIXTY_EIGHT/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace RMV_SIXTY_EIGHT
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
